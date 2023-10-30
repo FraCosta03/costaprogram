5adf130a5a476f9a6f38e171cb6753e898cf06e9
Francesco <fra.costat.11.03@gmail.com>
2023-10-30 16:08:06 +0100

    a franci ora va

--- run.py
+++ run.py
@@ -2,12 +2,4 @@ from flask import Flask, jsonify, make_response
 from http import HTTPStatus
 from src.app.__init__ import create_app
 
-app = create_app()
-
-#@app.route ('/welcome', methods=['GET'])
-@app.get ('/welcome')
-def welcome():
-    return 'welcome'
-@app.get('/login')
-def login():
-    return 'login'
+app = create_app('default')
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,4 +4,8 @@ from .config import config
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    from src.app.main.views import main
+    app.register_blueprint(main, url_prefix = '/main')
+
     return app
